089961ef25aa93e229ef33682b0662db31e3d4b5
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-07-01 18:23:09 +0700

    Add files via upload

---
+++ AutoClearLag.js
@@ -0,0 +1,5 @@
+setInterval(()=>{
+mc.runcmd("kill @e [type=item]");
+mc.runcmd("kill @e [type=xp_orb]")
+mc.broadcast("§c§lHệ Thống §f↣ Dọn Rác Thàng Công, Hệ Thống Sẽ Tiếp Tục Dọn Sau 500s Nữa", 0);
+}, 500000);
